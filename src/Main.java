d466d376e7cf31ddd52d56ca4f6c9accf5c195e4
Alexandra <saa-saa@mail.ru>
2025-02-24 23:47:34 +0300

    Поменял вывод

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        PrintStream printf = System.out.printf("Привет мир!");
+        PrintStream printf = System.out.printf("Привет, о дивный новый мир!");
     }
 }
